27d8e4b4d7f226dd0547b9812c7d2d965eb01d60
MaciejGajewski1 <mgajewski101@gmail.com>
2022-11-24 17:29:48 +0100

    Added redirect method in links controller. Modified opened link tables views to be ready to search for links ids.

--- src/main/java/projekt/wsb/linkbinder/service/LinkService.java
+++ src/main/java/projekt/wsb/linkbinder/service/LinkService.java
@@ -7,4 +7,5 @@ public interface LinkService {
     String deleteLink(String currentLink, String loggedUsername, Model model);
     String addLink(LinkDto linkDto, String tablename, Model model);
     String redirectToLinkTables(String tablename, Model model);
+    String getLink(final String id);
 }
